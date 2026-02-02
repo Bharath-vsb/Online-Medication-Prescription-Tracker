2fcc3bbce3df63b108e3f061748efd7076e912a5
Bharath-vsb <vbharathvsb@gmail.com>
2026-02-02 09:45:25 +0530

    delete

--- supabase/migrations/20260110105715_3a00ccae-7973-40f7-b60e-207d56ab264c.sql
+++
@@ -1,6 +0,0 @@
--- Allow pharmacists to update is_sold field on prescriptions
-CREATE POLICY "Pharmacists can update prescription sold status" 
-ON public.prescriptions 
-FOR UPDATE 
-USING (has_role(auth.uid(), 'pharmacist'::app_role))
-WITH CHECK (has_role(auth.uid(), 'pharmacist'::app_role));
